dee3b399caa86440cb15e19395a14f8b971f8677
Lye <lyrollorenzo@gmail.com>
2025-04-08 23:29:59 +0800

    - init

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/lye/demoproject/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.lye.demoproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @GetMapping("/helloWorld")
+    public String helloWord() {
+        return "Hello World Dev Tools Test GG";
+    }
+
+
+}
