f054b3ba90a91d66a6d2bf6dbaf8a3b2cfb9e523
Lye <lyrollorenzo@gmail.com>
2025-04-12 23:05:09 +0800

    - spring boot overview sample

--- 01-spring-boot-overview/05-properties-demo/src/main/java/com/lye/demoproject/controller/DemoController.java
+++ 01-spring-boot-overview/05-properties-demo/src/main/java/com/lye/demoproject/controller/DemoController.java
@@ -1,13 +1,23 @@
 package com.lye.demoproject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+    @Value("${team.team-name}")
+    private String teamName;
+
+    @Value("${team.coach-name}")
+    private String coachName;
+
     @GetMapping("/helloWorld")
     public String helloWord() {
         return "Hello World";
@@ -16,5 +26,14 @@ public class DemoController {
     @GetMapping("/devTools")
     public String autoReloading() {
         return "Application auto reloading...";
+    }
+
+    @GetMapping("/getTeamInfo")
+    public Object getTeamInfo() {
+        Map<String, String> team = new HashMap<>();
+        team.put("teamName", teamName);
+        team.put("coachName", coachName);
+
+        return team;
     }
 }
