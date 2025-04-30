6cf6966db151dc73c7c07c4719e9366ef158e811
Lye <lyrollorenzo@gmail.com>
2025-04-30 22:55:24 +0800

    update folder and file structure

---
+++ 05-spring-boot-jpa-advance/sql-scripts-employee/insert-employees.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (first_name, last_name, email) VALUES
+	('Lie', 'Me', 'lie.me@demo.com'),
+	('Arthur', 'Leywin', 'arthur.leywin@demo.com'),
+	('Sung', 'Jinwo', 'sung.jinwo@demo.com'),
+	('Lelouch', 'Britania', 'lelouch.britania@demo.com'),
+	('Que', 'Pal', 'que.pal@demo.com');
